758311d594b787eb6f87c3276275727e5cbdfc02
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-09-25 02:56:02 -0400

    add csv functionality

--- tools.py
+++ tools.py
@@ -1,18 +1,34 @@
+import os
+import csv
 import json
+import pandas as pd
 
 from pathlib import Path
 
 
-def get_credentials() -> list:
-    file = open(
-        "credentials/credentials.json",
-    )
+def get_credentials(path: str) -> list:
+    file = open(path)
     data = json.load(file)
     user, password = data["user"], data["password"]
     file.close()
     return [user, password]
 
 
-def get_signature() -> str:
-    txt = Path("signature/signature.html").read_text()
+def get_signature(path: str) -> str:
+    txt = Path(path).read_text()
     return txt.replace("\n", "")
+
+
+def read_csv(path: str) -> str:
+    return pd.read_csv(os.getcwd() + path)
+
+
+def generate_headers(row: list, path: str) -> dict:
+    headers = {
+        "email": row[0],
+        "recipient name": row[1],
+        "company name": row[2],
+        "contact name": row[3],
+        "package path": os.getcwd() + path,
+    }
+    return headers
